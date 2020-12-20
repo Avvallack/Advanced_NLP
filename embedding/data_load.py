- updates metric calucaltion - fixes gradient updates

embedding/data_load.py
@@ -6,7 +6,11 @@ from embedding.data_preparation import clean_title
 
 
 PATH = 'https://dl.dropboxusercontent.com/s/5qz8lb8nbp4dk0p/title_tags.csv?dl=0'
-DATA_FRAME = pd.read_csv(PATH, index_col=0)
+try:
+    DATA_FRAME = pd.read_csv('tags_titles.csv', index_col=0)
+except FileNotFoundError:
+    DATA_FRAME = pd.read_csv(PATH, index_col=0)
+    DATA_FRAME.to_csv('tags_titles.csv')
 DATA_FRAME['clean_tags'] = DATA_FRAME['clean_tags'].apply(ast.literal_eval)
 DATA_FRAME['clean_title'] = DATA_FRAME['clean_title'].apply(clean_title)
 DATA_FRAME['clean_tags'] = [tag if tag else np.nan for tag in DATA_FRAME.clean_tags]
@@ -14,13 +18,17 @@ DATA_FRAME['clean_title'] = [token if token else np.nan for token in DATA_FRAME.
 DATA_FRAME.dropna(inplace=True)
 DATA_FRAME.reset_index(drop=True, inplace=True)
 
+
 TEXT_PATH = 'https://dl.dropboxusercontent.com/s/glz13exvbzprtw3/titles_texts_habr.csv?dl=0'
-TEXT_FRAME = pd.read_csv(TEXT_PATH, index_col=0)
+try:
+    TEXT_FRAME = pd.read_csv('texts_titles.csv', index_col=0)
+except FileNotFoundError:
+    TEXT_FRAME = pd.read_csv(TEXT_PATH, index_col=0)
+    TEXT_FRAME.to_csv('texts_titles.csv')
+
 TEXT_FRAME['clean_title'] = TEXT_FRAME['clean_title'].apply(ast.literal_eval)
 TEXT_FRAME['clean_text'] = TEXT_FRAME['clean_text'].apply(ast.literal_eval)
 TEXT_FRAME['clean_text'] = [token if token else np.nan for token in TEXT_FRAME.clean_text]
 TEXT_FRAME['clean_title'] = [token if token else np.nan for token in TEXT_FRAME.clean_title]
 TEXT_FRAME.dropna(inplace=True)
 TEXT_FRAME.reset_index(drop=True, inplace=True)
-
-SIMPLE_TITLE = DATA_FRAME['clean_title'].apply(lambda x: x[0])

embedding/embedding_model.py
@@ -1,27 +1,26 @@
 import numpy as np
 import pandas as pd
-from numpy.random import normal
-from random import choice, choices
 from tqdm.notebook import tqdm
+from scipy.sparse import csr_matrix
 
 from embedding.configuration import *
 
 
-def initialize_embedding(vocabulary, dimension=300, variance=0.01):
-    return normal(loc=0, scale=variance, size=(len(vocabulary) + 1, dimension)).astype(np.float32) + 0.00000001
+def initialize_embedding(vocabulary, dimension=300):
+    return np.random.randn(len(vocabulary), dimension).astype(np.float32) * 1e-3
 
 
 def loss_function(train_vector, truth_target, wrong_target):
     return max(0, (1 - train_vector.dot(truth_target) + train_vector.dot(wrong_target)))
 
 
-def gradient(train_vector, truth_target, wrong_target):
-    loss = loss_function(train_vector, truth_target, wrong_target)
+def gradient(anchor, truth_target, wrong_target):
+    loss = loss_function(anchor, truth_target, wrong_target)
     if loss == 0:
         return 0, []
     anchor_derivative = - truth_target + wrong_target
-    positive_derivative = - train_vector
-    negative_derivative = train_vector
+    positive_derivative = - anchor
+    negative_derivative = anchor
     return loss, [anchor_derivative, positive_derivative, negative_derivative]
 
 
@@ -30,8 +29,6 @@ def get_gradient_norm(gradient_vector):
 
 
 def choose_triplet(index_frame, anchor_index, wrong_index, train_col=TITLE_COL, target_col=TAGS_COL):
-    """ shuffle index and get cyclic shift for 1 step numpy roll"""
-
     anchor = index_frame.iloc[anchor_index][train_col]
     positive = index_frame.iloc[anchor_index][target_col]
     negative = index_frame.iloc[wrong_index][target_col]
@@ -47,25 +44,31 @@ def get_avg_vector(vector, embedding_matrix):
     return embedding_matrix[vector].mean(axis=0), vector
 
 
+def get_documents_matrix(dataframe_column, embedding_matrix, vector_type):
+    n_row = len(dataframe_column)
+    dataframe_column = dataframe_column.reset_index(drop=True).explode()
+    sparse_col = csr_matrix(
+        (np.ones(len(dataframe_column)),
+         (dataframe_column.index.values,
+          dataframe_column.values)),
+        shape=(n_row, embedding_matrix.shape[0])
+    )
+    embed_vectors = sparse_col.dot(embedding_matrix)
+    if vector_type == 'sum':
+        return
+    return embed_vectors / sparse_col.sum(axis=1)
+
+
 def get_title_tags_similarity_matrix(index_frame: pd.DataFrame,
                                      embedding_matrix: np.ndarray,
                                      train_col=TITLE_COL,
                                      target_col=TAGS_COL,
-                                     batch_size=10000,
+                                     test_size=10000,
                                      vector_type='sum'):
-    title_matrix = np.zeros((batch_size, embedding_matrix.shape[1]), dtype=np.float32)
-    tags_matrix = np.zeros((batch_size, embedding_matrix.shape[1]), dtype=np.float32)
-    indices = choices(index_frame.index, k=batch_size)
-    for index, row in index_frame.iloc[indices].reset_index(drop=True).iterrows():
-        if vector_type == 'sum':
-            title_matrix[index] = embedding_matrix[row[train_col]].sum(axis=0)
-            tags_matrix[index] = embedding_matrix[row[target_col]].sum(axis=0)
-        elif vector_type == 'avg':
-            title_matrix[index] = embedding_matrix[row[train_col]].mean(axis=0)
-            tags_matrix[index] = embedding_matrix[row[target_col]].mean(axis=0)
-        else:
-            raise ValueError
-    return title_matrix.dot(tags_matrix.T)
+    train_docs = get_documents_matrix(index_frame[train_col].tail(test_size), embedding_matrix, vector_type)
+    target_docs = get_documents_matrix(index_frame[target_col].tail(test_size), embedding_matrix, vector_type)
+    similarity = train_docs.dot(target_docs.T)
+    return similarity
 
 
 def get_most_similar_k(title_tags_matrix, k=10):
@@ -74,8 +77,7 @@ def get_most_similar_k(title_tags_matrix, k=10):
 
 def calculate_metric(most_similar_matrix):
     zeros = most_similar_matrix == np.arange(most_similar_matrix.shape[0])[:, None]
-    sums = zeros.sum(axis=1)
-    return sums.mean()
+    return zeros.mean()
 
 
 def gradient_vector_choice(gradient_vector, indices, vector_type='sum'):
@@ -83,11 +85,11 @@ def gradient_vector_choice(gradient_vector, indices, vector_type='sum'):
         return gradient_vector
     if vector_type == 'avg':
         return gradient_vector / len(indices)
-    raise ValueError
+    raise ValueError('Incorrect vector method was passed')
 
 
 def adagrad_update(gradient_vector, gradient_matrix, index_list):
-    return gradient_matrix[index_list] + np.squre(gradient_vector)
+    return gradient_matrix[index_list] + np.square(gradient_vector)
 
 
 def rmsprop_update(gradient_vector, gradient_matrix, index_list, update_gamma=0.95):
@@ -103,23 +105,24 @@ def train_embeddings(index_frame,
                      stop_rounds=5,
                      train_col=TITLE_COL,
                      target_col=TAGS_COL,
-                     update_rate=None):
-    gradient_matrix = np.zeros(embedding_matrix.shape, dtype=np.float32)
+                     update_rate=None,
+                     test_size=5000,
+                     k=5):
+    gradient_matrix = np.full_like(embedding_matrix, 1e-8,  dtype=np.float32)
     best_embeddings = embedding_matrix.copy()
     best_metric = 0
     unchanged_rounds = 0
-    vector_norms = []
     if vector_method == 'sum':
         vector_function = get_sum_vector
-    else:
+    elif vector_method == 'avg':
         vector_function = get_avg_vector
+    else:
+        raise ValueError('Incorrect vector method was passed')
     for _ in tqdm(range(num_iterations)):
-        epoch_norms = []
-        shuffled_index = index_frame.index.values.copy()
-        np.random.shuffle(shuffled_index)
-        shifted_index = np.roll(shuffled_index, 1)
-        for right_idx, wrong_idx in tqdm(zip(shuffled_index, shifted_index)):
-            triplet = choose_triplet(index_frame, right_idx, wrong_idx, train_col, target_col)
+        true_idx = np.random.permutation(index_frame.shape[0] - test_size)
+        false_idx = np.roll(true_idx, 1)
+        for true_index, false_index in zip(true_idx, false_idx):
+            triplet = choose_triplet(index_frame, true_index, false_index, train_col, target_col)
             indices_list = []
             sum_vectors = []
             for vector in triplet:
@@ -128,20 +131,24 @@ def train_embeddings(index_frame,
                 sum_vectors.append(sum_vector)
 
             loss, gradient_vector = gradient(*sum_vectors)
-            epoch_norms.append(get_gradient_norm(gradient_vector))
             for grad, idx in zip(gradient_vector, indices_list):
-                embedding_matrix[idx] -= (learning_rate * grad) / np.sqrt(gradient_matrix[idx] + 0.00000001)
                 grad = gradient_vector_choice(grad, idx, vector_method)
                 if descent_type == 'adagrad':
                     gradient_matrix[idx] = adagrad_update(grad, gradient_matrix, idx)
                 elif descent_type == 'rmsprop' and update_rate:
                     gradient_matrix[idx] = rmsprop_update(grad, gradient_matrix, idx, update_rate)
                 else:
-                    print('wrong combination')
-                    raise ValueError
-        vector_norms.append(np.array([elem for elem in epoch_norms if elem]).mean(axis=0))
-        doc_matrix = get_title_tags_similarity_matrix(index_frame, embedding_matrix, target_col, train_col)
-        most_similar = get_most_similar_k(doc_matrix, k=10)
+                    raise ValueError('Incorrect optimization method was passed')
+
+                embedding_matrix[idx] -= (learning_rate * grad) / np.sqrt(gradient_matrix[idx])
+
+        doc_matrix = get_title_tags_similarity_matrix(index_frame,
+                                                      embedding_matrix,
+                                                      target_col,
+                                                      train_col,
+                                                      test_size,
+                                                      vector_method)
+        most_similar = get_most_similar_k(doc_matrix, k=k)
         metric = calculate_metric(most_similar)
         if metric > best_metric:
             best_embeddings = embedding_matrix.copy()
@@ -154,7 +161,7 @@ def train_embeddings(index_frame,
         if unchanged_rounds >= stop_rounds:
             print("Current metric is: {:.3f}".format(best_metric))
             break
-    return best_embeddings, vector_norms
+    return best_embeddings
 
 
 if __name__ == '__main__':